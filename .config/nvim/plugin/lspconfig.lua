8041b2f479769e3341719163276b146bc10285df
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2023-09-23 20:07:04 +0700

    🐛 fix(nvim): add comment

--- .config/nvim/plugin/lspconfig.lua
+++ .config/nvim/plugin/lspconfig.lua
@@ -72,6 +72,7 @@ nvim_lsp.flow.setup {
   capabilities = capabilities
 }
 
+-- TypeScript
 nvim_lsp.tsserver.setup {
   on_attach = on_attach,
   filetypes = { "typescript", "typescriptreact", "typescript.tsx" },
@@ -84,6 +85,7 @@ nvim_lsp.sourcekit.setup {
   capabilities = capabilities,
 }
 
+-- Lua
 nvim_lsp.lua_ls.setup {
   capabilities = capabilities,
   on_attach = function(client, bufnr)
