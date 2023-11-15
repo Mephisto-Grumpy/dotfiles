7e0907b15f4fb6d872372f88f64d75f1f5890d9d
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2023-11-15 14:21:15 +0700

    ✨ feature(nvim): using new neovim with LazyVim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,23 +1,11 @@
-require('mephisto.base')
-require('mephisto.maps')
-require('mephisto.plugins')
-require('mephisto.highlights')
+if vim.loader then
+	vim.loader.enable()
+end
 
-local has = vim.fn.has
-local is_mac = has "macunix"
-local is_linux = has "unix"
-local is_win = has "win32"
-local is_wsl = has "wsl"
+_G.dd = function(...)
+	require("util.debug").dump(...)
+end
 
-if is_mac then
-  require('mephisto.macos')
-end
-if is_linux then
-  require('mephisto.linux')
-end
-if is_win then
-  require('mephisto.windows')
-end
-if is_wsl then
-  require('mephisto.wsl')
-end
+vim.print = _G.dd
+
+require("config.lazy")
