9ede5e96ad7e621363a764ab9a5b28d77f506209
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2024-01-22 20:13:28 +0700

    🐛 fix(nvim): change `python-semshi` to `python`

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -34,7 +34,7 @@ require("lazy").setup({
 		{ import = "lazyvim.plugins.extras.lang.json" },
 		{ import = "lazyvim.plugins.extras.lang.rust" },
 		{ import = "lazyvim.plugins.extras.lang.go" },
-		{ import = "lazyvim.plugins.extras.lang.python-semshi" },
+		{ import = "lazyvim.plugins.extras.lang.python" },
 		{ import = "lazyvim.plugins.extras.lang.tailwind" },
 		{ import = "lazyvim.plugins.extras.lang.terraform" },
 		{ import = "lazyvim.plugins.extras.coding.copilot" },
