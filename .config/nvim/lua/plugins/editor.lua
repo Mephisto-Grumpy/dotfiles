4daebdb24747a47fbc4bc7d5ca9ae9e9b1800891
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2024-01-29 21:12:38 +0700

    🐛 fix(editor): add file util and wrong pattern

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -21,8 +21,9 @@ return {
 				hsl_color = {
 					pattern = "hsl%(%d+,? %d+%%?,? %d+%%?%)",
 					group = function(_, match)
+						local utils = require("dullahan.hsl")
 						--- @type string, string, string
-						local nh, ns, nl = match:match("hsl%((%d+),? (%d+)%%?,? (%d+)%%? %)")
+						local nh, ns, nl = match:match("hsl%((%d+),? (%d+)%%?,? (%d+)%%?%)")
 						--- @type number?, number?, number?
 						local h, s, l = tonumber(nh), tonumber(ns), tonumber(nl)
 						--- @type string
