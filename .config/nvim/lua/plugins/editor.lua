be6b61d010a7f391f778a695fdb160379d6193a3
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2024-01-29 20:21:44 +0700

    🐛 fix(editor): highlighting `hsl` colors not working on CSS

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -19,10 +19,10 @@ return {
 		opts = {
 			highlighters = {
 				hsl_color = {
-					pattern = "hsl%(%d+,? %d+,? %d+%)",
+					pattern = "hsl%(%d+,? %d+%%?,? %d+%%?%)",
 					group = function(_, match)
 						--- @type string, string, string
-						local nh, ns, nl = match:match("hsl%((%d+),? (%d+),? (%d+)%)")
+						local nh, ns, nl = match:match("hsl%((%d+),? (%d+)%%?,? (%d+)%%? %)")
 						--- @type number?, number?, number?
 						local h, s, l = tonumber(nh), tonumber(ns), tonumber(nl)
 						--- @type string
