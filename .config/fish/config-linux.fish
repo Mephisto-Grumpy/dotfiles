ad160bf83f148e2fb81ecd2803783593059d6a9a
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2024-10-07 02:53:45 +0700

    feat(wsl): add config for wsl

--- .config/fish/config-linux.fish
+++ .config/fish/config-linux.fish
@@ -3,3 +3,12 @@ if test -x /home/linuxbrew/.linuxbrew/bin/brew
     eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
 end
 
+# WSL
+if test -n "$WSL_DISTRO_NAME"
+    alias open wslview
+    alias c: "cd /mnt/c"
+    alias d: "cd /mnt/d"
+
+    # Remove cursor from PATH (it's a symlink to /mnt/c/Users/PunGrumpy/AppData/Local/Programs/cursor/resources/app/bin)
+    set -gx PATH (string match -v '/mnt/c/Users/PunGrumpy/AppData/Local/Programs/cursor/resources/app/bin' $PATH)
+end
